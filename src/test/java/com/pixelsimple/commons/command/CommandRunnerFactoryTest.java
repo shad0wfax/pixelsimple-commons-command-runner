8abe5f10c7e65341ceb417a5da3b40484d65a934
Akshay Sharma <cto@pixelsimple.com>
2011-12-01 22:18:47 +0530

    fixed the cobertura coverage issue - note: issue fixed by upgrading to version 1.9.4.1 / Added new test

---
+++ src/test/java/com/pixelsimple/commons/command/CommandRunnerFactoryTest.java
@@ -0,0 +1,56 @@
+/**
+ * © PixelSimple 2011-2012.
+ */
+package com.pixelsimple.commons.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pixelsimple.commons.command.callback.AsyncCallbackHandler;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Akshay Sharma
+ * Dec 1, 2011
+ */
+public class CommandRunnerFactoryTest extends TestCase {
+	static final Logger LOG = LoggerFactory.getLogger(CommandRunnerFactoryTest.class);
+
+	/**
+	 * Test method for {@link com.pixelsimple.commons.command.CommandRunnerFactory#newBlockingCommandRunner()}.
+	 */
+	public void testNewBlockingCommandRunner() {
+		CommandRunner runner = CommandRunnerFactory.newBlockingCommandRunner();
+		Assert.assertNotNull(runner);
+	}
+
+	/**
+	 * Test method for {@link com.pixelsimple.commons.command.CommandRunnerFactory#newAsyncCommandRunner(com.pixelsimple.commons.command.callback.AsyncCallbackHandler)}.
+	 */
+	public void testNewAsyncCommandRunner() {
+		CommandRunner runner = CommandRunnerFactory.newAsyncCommandRunner(null);
+		Assert.assertNotNull(runner);
+		
+		runner = CommandRunnerFactory.newAsyncCommandRunner(anonAsyncHandler());
+		Assert.assertNotNull(runner);
+	}
+	
+	private AsyncCallbackHandler anonAsyncHandler() {
+		return (new AsyncCallbackHandler() {
+			
+			@Override
+			public void onCommandFailed(CommandRequest commandRequest, CommandResponse commandResponse) {
+				LOG.debug("testNewAsyncCommandRunner::Command Failed");
+			}
+			
+			@Override
+			public void onCommandComplete(CommandRequest commandRequest, CommandResponse commandResponse) {
+				LOG.debug("testNewAsyncCommandRunner::Command Succeeded");
+			}
+		});
+	}
+
+}
--- src/test/java/com/pixelsimple/commons/command/handler/CommandResponseHandlerTest.java
+++ src/test/java/com/pixelsimple/commons/command/handler/CommandResponseHandlerTest.java
@@ -29,7 +29,7 @@ public class CommandResponseHandlerTest extends TestCase {
 		CommandResponseHandler handler = new CommandResponseHandler(response);
 		handler.processLine("hello world", 1);
 		LOG.debug("Response form Command::{}", response.getSuccessResponse());
-		Assert.assertTrue(true);
+		Assert.assertEquals(response.getSuccessResponse().toString(), "hello world");
 	}
 
 }
