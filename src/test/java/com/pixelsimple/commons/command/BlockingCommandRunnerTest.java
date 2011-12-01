31c7ccffa200feb5ecbb6a20e404ec54c5ef8805
Akshay Sharma <cto@pixelsimple.com>
2011-12-02 02:13:25 +0530

    Added more tests. Improve coverage

---
+++ src/test/java/com/pixelsimple/commons/command/BlockingCommandRunnerTest.java
@@ -0,0 +1,61 @@
+/**
+ * © PixelSimple 2011-2012.
+ */
+package com.pixelsimple.commons.command;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.apache.commons.exec.ExecuteException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Akshay Sharma
+ * Dec 2, 2011
+ */
+public class BlockingCommandRunnerTest extends TestCase {
+	static final Logger LOG = LoggerFactory.getLogger(BlockingCommandRunnerTest.class);
+
+	/**
+	 * Test method for {@link com.pixelsimple.commons.command.AbstractCommandRunner#runCommand(com.pixelsimple.commons.command.CommandRequest, com.pixelsimple.commons.command.CommandResponse)}.
+	 */
+	public void testRunCommandSimpleSuccess() {
+		BlockingCommandRunner runner = new BlockingCommandRunner();
+		CommandResponse response = new CommandResponse();
+		CommandRequest request = CommandUtilForTest.simpleCommand();
+		
+		// TODO: This will block - how to test the blockage???
+		runner.runCommand(request, response);
+		
+		Assert.assertNotNull(response.getSuccessResponse());
+		Assert.assertNull(response.getFailureResponse());
+		Assert.assertEquals(response.getCommandExitValueObtained(), request.getCommandExitValue());
+		Assert.assertEquals(response.hasCommandFailed(), Boolean.FALSE);
+		
+		// TODO: Add more meaningful assert - how to assert the output?
+	}
+
+
+	/**
+	 * Test method for {@link com.pixelsimple.commons.command.AbstractCommandRunner#runCommand(com.pixelsimple.commons.command.CommandRequest, com.pixelsimple.commons.command.CommandResponse)}.
+	 */
+	public void testRunCommandSimpleFailureWithWrongReturnCodeExecuteException() {
+		BlockingCommandRunner runner = new BlockingCommandRunner();
+		CommandResponse response = new CommandResponse();
+		CommandRequest request = CommandUtilForTest.simpleCommandWithWrongExitValue();
+		
+		// TODO: This will block - how to test the blockage???
+		runner.runCommand(request, response);
+		
+		// Since the error occurs after the command has run for a while, the success response is NOT NULL. 
+//		Assert.assertNull(response.getSuccessResponse());
+		Assert.assertNotNull(response.getFailureResponse());
+		Assert.assertEquals(response.hasCommandFailed(), Boolean.TRUE);
+		Assert.assertNotNull(response.getFailureCause());
+		Assert.assertEquals(response.getFailureCause().getClass(), ExecuteException.class);
+		
+		// TODO: Add more meaningful assert - how to assert the output?
+	}
+}
---
+++ src/test/java/com/pixelsimple/commons/command/CommandUtilForTest.java
@@ -0,0 +1,65 @@
+/**
+ * © PixelSimple 2011-2012.
+ */
+package com.pixelsimple.commons.command;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pixelsimple.commons.command.callback.AsyncCallbackHandler;
+
+/**
+ *
+ * @author Akshay Sharma
+ * Dec 2, 2011
+ */
+public class CommandUtilForTest extends TestCase {
+	static final Logger LOG = LoggerFactory.getLogger(CommandUtilForTest.class);
+	
+	public void testDummy() {
+		Assert.assertTrue("Just a dummy test that needs to be there in this class for junit. DO NOT REMOVE.", true);
+	}
+
+	public static AsyncCallbackHandler anonAsyncHandler() {
+		return (new AsyncCallbackHandler() {
+			
+			@Override
+			public void onCommandFailed(CommandRequest commandRequest, CommandResponse commandResponse) {
+				LOG.debug("testNewAsyncCommandRunner::Command Failed");
+			}
+			
+			@Override
+			public void onCommandComplete(CommandRequest commandRequest, CommandResponse commandResponse) {
+				LOG.debug("testNewAsyncCommandRunner::Command Succeeded");
+			}
+		});
+	}
+	
+	public static CommandRequest simpleCommand() {
+		CommandRequest request = new CommandRequest();
+		request.addCommand("ls -l", 0);
+		return request;
+	}
+	
+	/**
+	 * Very well known that ls -l command should exit with 0. If it is to exit with 1, it throws an exception. 
+	 * @return
+	 */
+	public static CommandRequest simpleCommandWithWrongExitValue() {
+		CommandRequest request = new CommandRequest();
+		request.addCommand("ls -l", 1);
+		return request;
+	}
+	
+//	public static CommandResponse simpleCommandResponse() {
+//		CommandResponse response = new CommandResponse();
+//		response.gatherSuccessResponse("")
+//		
+//		return response;
+//	}
+
+
+}
